c5e1747ba2a9a07cd88439de3699b7eb76c7ea39
Hu3diger <30418918+Hu3diger@users.noreply.github.com>
2018-05-24 11:28:29 -0300

    first

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBasics
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Object principal;
+        Page1 page1;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            page1 = new Page1(300, this);
+            page1.Background = Brushes.BlueViolet;
+            
+            principal = this.Content;
+        }
+
+        private void goToRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Register rg = new Register();
+            this.Close ();
+            rg.Show();
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void gotoProcess_Click(object sender, RoutedEventArgs e)
+        {
+            this.Content = page1;
+        }
+
+        public void goBack()
+        {
+            this.Content = principal;
+            TextBlock txt = new TextBlock();
+            txt.Text = page1.apareceAqui.Text;
+            txt.Foreground = new SolidColorBrush(Colors.Yellow);
+            TextBlock txt1 = new TextBlock();
+            txt1.Text = page1.apareceAqui.Text;
+            txt1.Foreground = new SolidColorBrush(Colors.Pink);
+            StackPanel stck = new StackPanel();
+            stck.Children.Add(txt);
+            stck.Children.Add(txt1);
+            this.viewer.Content = stck;
+        }
+
+        /*
+        private void Teste_Click(object sender, RoutedEventArgs e)
+        {
+            Window win = new Window();
+            win.Height = 150;
+            TextBox txtBox = new TextBox();
+            txtBox.Text = "Teste";
+            StackPanel stck = new StackPanel();
+            Button btn = new Button();
+            btn.Width = 200;
+            btn.Height = 200;
+            stck.Children.Add(txtBox);
+            stck.Children.Add(btn);
+            //Border myborder = new Border();
+            win.Content = stck;
+            win.Show();
+        }
+        */
+    }
+}
---
+++ WpfApp1/Page1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfBasics
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        MainWindow _window;
+        public Page1(int teste, MainWindow window)
+        {
+            InitializeComponent();
+            this.Height = teste;
+            _window = window;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _window.goBack();
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            apareceAqui.Text = escreveAqui.Text;
+        }
+    }
+}
